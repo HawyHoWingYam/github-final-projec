ae1e882bba0be98df26bff002764426a8b9d81c1
jfupoly <22012231g@connect.polyu.hk>
2024-04-01 06:37:49 +0800

    preprocessing, select columns, outliers

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -2,24 +2,23 @@ import pandas as pd
 import numpy as np
 DATA_PATH = "../data/"
 PROCESSED_DATA_PATH = "../processed_data/"
-COLS_TO_REMOVE = ['LowQualFinSF', 'MiscFeature', 'YearBuilt', 'YrSold', 'PoolQC', 'PoolArea', 'GarageYrBlt', 'YearRemodAdd', 'BsmtUnfSF']
-FILLNA_DICT = {
-    'MasVnrArea': 0.,
-    'Electrical': 'SBrkr',
-    'LotFrontage': 0.,
-    'BsmtQual': 'NA',
-    'BsmtCond': 'NA',
-    'BsmtExposure': 'NA',
-    'BsmtFinType1': 'NA',
-    'BsmtFinType2': 'NA',
-    'GarageFinish': 'NA',
-    'GarageQual': 'NA',
-    'GarageCond': 'NA',
-    'GarageType': 'NA',
-    'FireplaceQu': 'NA',
-    'MasVnrType': 'NA',
-    'Fence': 'NA',
-    'Alley': 'NA',
+
+COLS_TO_SELECT = ['MSZoning', 'LotFrontage', 'LotArea', 'LotShape', 'Neighborhood',
+                  'Condition1', 'BldgType', 'HouseStyle', 'TotalBsmtSF', 'Foundation', 'CentralAir',
+                  'GrLivArea', 'BedroomAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 'TotalBath',
+                  'GarageCars', 'GarageArea', 'WasRemodeled', 'AgeWhenSold']
+
+LIMITS = {
+    'LotFrontage': (0, 150),
+    'LotArea': (0, 30000),
+    'TotalBsmtSF': (0, 2000),
+    'GrLivArea': (0, 3000),
+    'BedroomAbvGr': (1, 5),
+    'TotRmsAbvGrd': (2, 10),
+    'Fireplaces': (0, 2),
+    'TotalBath': (1, 4),
+    'GarageArea': (0, 1000),
+    'AgeWhenSold': (0, 100),
 }
 
 def get_datasets(split_ratio=0.7, seed=0):
@@ -31,10 +30,10 @@ def get_datasets(split_ratio=0.7, seed=0):
     test_df = pd.read_csv(DATA_PATH+"test.csv") # for Kaggle submission, no SalePrice
     return train_df, valid_df, test_df
 
-def get_processed_datasets():
-    train_df = pd.read_csv(PROCESSED_DATA_PATH+'train_processed.csv', index_col=0, keep_default_na=False)
-    valid_df = pd.read_csv(PROCESSED_DATA_PATH+'valid_processed.csv', index_col=0, keep_default_na=False)
-    test_df = pd.read_csv(PROCESSED_DATA_PATH+'test_processed.csv', index_col=0, keep_default_na=False)
+def get_processed_datasets(suffix=''):
+    train_df = pd.read_csv(PROCESSED_DATA_PATH+'train_processed'+suffix+'.csv', index_col=0, keep_default_na=False)
+    valid_df = pd.read_csv(PROCESSED_DATA_PATH+'valid_processed'+suffix+'.csv', index_col=0, keep_default_na=False)
+    test_df = pd.read_csv(PROCESSED_DATA_PATH+'test_processed'+suffix+'.csv', index_col=0, keep_default_na=False)
     return train_df, valid_df, test_df
 
 def get_features(df):
@@ -46,14 +45,14 @@ def get_features(df):
 # TODO: filter outliers from train_df
 def preprocess_dataset(df, is_testing=False):
     df = df.set_index('Id')
-    df['MSSubClass'] = 'Class ' + df.MSSubClass.astype(str)
     df['WasRemodeled'] = np.where(df.YearRemodAdd != df.YearBuilt, 'Y', 'N')
     df['AgeWhenSold'] = df.YrSold - df.YearBuilt
+    df['TotalBath'] = df['FullBath'] + df['BsmtFullBath'] + df['HalfBath'] + df['BsmtHalfBath']
 
-    df = df.drop(columns=COLS_TO_REMOVE)
+    df = df[COLS_TO_SELECT] if is_testing else df[COLS_TO_SELECT+['SalePrice']]
 
     # handle null values
-    df = df.fillna(FILLNA_DICT)
+    df['LotFrontage'] = df.LotFrontage.fillna(0.)
     numeric_features, category_features = get_features(df)
     # in case unseen data has unexpected null values
     for c in numeric_features+category_features:
@@ -62,38 +61,40 @@ def preprocess_dataset(df, is_testing=False):
         else:
             df[c] = df[c].fillna(df[c].mode()[0])
 
-    rating_map1 = {'Ex': 4, 'Gd': 3, 'TA': 2, 'Fa': 1, 'Po': 0, 'NA': 0}
-    rating_map2 = {'Gd': 4, 'Av': 3, 'Mn': 2, 'No': 1, 'NA': 0}
-    lst = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'HeatingQC', 'KitchenQual', 'FireplaceQu', 'GarageQual', 'GarageCond']
-    
-    df[lst] = df[lst].replace(rating_map1)
-    df['BsmtExposure'] = df.BsmtExposure.replace(rating_map2)
+    for k, (mini, maxi) in LIMITS.items():
+        df[k] = df[k].clip(mini, maxi)
 
     if not is_testing:
         df = pd.concat([df.drop(columns='SalePrice'), df['SalePrice']], axis=1)
     return df
 
-def replace_category_with_dummy_datasets(train_df, valid_df, test_df):
+def replace_category_with_dummy_datasets(train_df, valid_df, test_df, to_drop):
     _, train_category_features = get_features(train_df)
     mode = train_df[train_category_features].mode().iloc[0]
 
-    def replace_dummy(df, mode):
+    def replace_dummy(df, mode, to_print=False):
         _, category_features = get_features(df)
         df = pd.concat([pd.get_dummies(df[category_features], prefix=category_features), df], axis=1)
         df = df.drop(columns=category_features)
         df = df.astype(np.float64)
-        # drop each first category to avoid multicollinearity
-        cols = [c for c in df.columns if '_' in c]
-        for c in cols:
-            feature, category = c.split('_')
-            if category in ['Y', 'N']:
-                if category=='N':
-                    df = df.drop(columns=c)
-            else:
-                if category==mode[feature]:
-                    df = df.drop(columns=c)
+        # drop each most frequent category to avoid multicollinearity, relevant for regression models
+        if to_drop:
+            cols = [c for c in df.columns if '_' in c]
+            dropping = []
+            for c in cols:
+                feature, category = c.split('_')
+                if category in ['Y', 'N']:
+                    if category=='N':
+                        dropping.append(c)
+                else:
+                    if category==mode[feature]:
+                        dropping.append(c)
+            df = df.drop(columns=dropping)
+            if to_print:
+                print('dropped:')
+                print('\n'.join(dropping))
         return df
-    train_df = replace_dummy(train_df, mode)
+    train_df = replace_dummy(train_df, mode, to_print=True)
     valid_df = replace_dummy(valid_df, mode)
     test_df = replace_dummy(test_df, mode)
 
